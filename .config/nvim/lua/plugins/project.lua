b34d03553c138a0a107a32beb097db4cbe1a8703
HenryLie <11569345+HenryLie@users.noreply.github.com>
2024-01-11 23:55:35 +0900

    feat: add project.nvim

---
+++ .config/nvim/lua/plugins/project.lua
@@ -0,0 +1,7 @@
+return {
+  "ahmedkhalf/project.nvim",
+  opts = {
+    manual_mode = false,
+    patterns = { "!.zshrc", ".git", "_darcs", ".hg", ".bzr", ".svn", "Makefile", "package.json", "init.lua" },
+  },
+}
