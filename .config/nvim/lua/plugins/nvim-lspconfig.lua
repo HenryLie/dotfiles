4c33e582a13cc96a8be2959580618923da62f1c7
HenryLie <11569345+HenryLie@users.noreply.github.com>
2024-04-30 15:52:45 +0900

    feat: tempfix for svelte lsp ts autodetect

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,20 @@
+return {
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      svelte = {
+        on_attach = function(client)
+          if client.name == "svelte" then
+            vim.api.nvim_create_autocmd("BufWritePost", {
+              pattern = { "*.js", "*.ts" },
+              group = vim.api.nvim_create_augroup("svelte_ondidchangetsorjsfile", { clear = true }),
+              callback = function(ctx)
+                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+              end,
+            })
+          end
+        end,
+      },
+    },
+  },
+}
