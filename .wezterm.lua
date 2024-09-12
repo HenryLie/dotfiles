5e3ce8f0f7034ff81fce379cd5bf858336b043a8
HenryLie <11569345+HenryLie@users.noreply.github.com>
2024-09-13 01:17:55 +0900

    config: update wezterm config for wsl

--- .wezterm.lua
+++ .wezterm.lua
@@ -15,20 +15,26 @@ end
 -- For example, changing the color scheme:
 -- config.color_scheme = "AdventureTime"
 config.window_close_confirmation = "NeverPrompt"
+
 config.window_background_opacity = 0.75
 config.enable_tab_bar = false
 config.freetype_load_target = "Light"
 config.freetype_render_target = "Normal"
 config.freetype_load_flags = "NO_HINTING"
 config.font = wezterm.font_with_fallback({ "LigaHack Nerd Font", { family = "Noto Sans JP" } })
-config.font_size = 10
+config.font_size = 9
 config.front_end = "WebGpu"
+config.window_decorations = "RESIZE"
 config.window_padding = {
 	left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
+
+if wezterm.target_triple == "aarch64-apple-darwin" then
+	config.font_size = 10
+end
 
 -- and finally, return the configuration to wezterm
 return config
