e91ed8917cc53836e762d764e4bead8d0686f747
HenryLie <11569345+HenryLie@users.noreply.github.com>
2024-12-03 16:15:54 +0900

    fix(lazyvim): exit term, resize window keys

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -25,12 +25,12 @@ map("n", "<C-Up>", "<C-w>k", { desc = "Go to upper window", remap = true })
 map("n", "<C-Right>", "<C-w>l", { desc = "Go to right window", remap = true })
 
 -- Move Lines
-map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
-map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
-map("i", "<A-Don>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
-map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
-map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
-map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
+-- map("n", "<A-Down>", "<cmd>m .+1<cr>==", { desc = "Move down" })
+-- map("n", "<A-Up>", "<cmd>m .-2<cr>==", { desc = "Move up" })
+-- map("i", "<A-Don>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+-- map("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+-- map("v", "<A-Down>", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+-- map("v", "<A-Up>", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- buffers
 if Util.has("bufferline.nvim") then
@@ -40,3 +40,11 @@ else
   map("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev buffer" })
   map("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next buffer" })
 end
+
+-- Resize windows
+map("n", "<C-A-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-A-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-A-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+map("n", "<C-A-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+map("t", "<esc><esc>", "<C-\\><C-N>", { desc = "Exit terminal focus" })
