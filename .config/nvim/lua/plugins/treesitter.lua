f4fd6ccb20c66be9b6edd752353785e6ab3164ce
HenryLie <11569345+HenryLie@users.noreply.github.com>
2025-03-28 16:15:50 +0900

    chore(treesitter): populate ensure_installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,8 +7,21 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "css",
+        "bash",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "html",
+        "javascript",
+        "jsdoc",
+        "json",
+        "lua",
+        "markdown",
         "scss",
+        "styled",
         "svelte",
+        "typescript",
+        "tsx",
         "vue",
       })
     end,
