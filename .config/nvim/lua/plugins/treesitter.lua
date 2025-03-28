23fce1f47ae903417ff23dce92792d3a1d8d0700
HenryLie <11569345+HenryLie@users.noreply.github.com>
2025-03-28 22:26:14 +0900

    refactor(treesitter): remove lazy default from ensure_installed

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -7,21 +7,11 @@ return {
     opts = function(_, opts)
       vim.list_extend(opts.ensure_installed, {
         "css",
-        "bash",
-        "diff",
         "dockerfile",
         "git_config",
-        "html",
-        "javascript",
-        "jsdoc",
-        "json",
-        "lua",
-        "markdown",
         "scss",
         "styled",
         "svelte",
-        "typescript",
-        "tsx",
         "vue",
       })
     end,
