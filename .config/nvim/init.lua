3fe20b01b2de597309ec607c079cb1795fd93e08
HenryLie <11569345+HenryLie@users.noreply.github.com>
2026-01-22 13:57:31 +0900

    refactor: experimental md formatter cnfig

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,2 +1,25 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.frontmatter = {
+  install_info = {
+    url = "~/projects/tree-sitter-frontmatter", -- local path or git repo
+    files = { "src/parser.c", "src/scanner.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "main", -- default branch in case of git repo if different from master
+    generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+  },
+}
+
+parser_config.markdown = {
+  install_info = {
+    url = "~/projects/tree-sitter-markdown", -- local path or git repo
+    files = { "src/parser.c", "src/scanner.cc" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+    -- optional entries:
+    branch = "master", -- default branch in case of git repo if different from master
+    generate_requires_npm = true, -- if stand-alone parser without npm dependencies
+    requires_generate_from_grammar = true, -- if folder contains pre-generated src/parser.c
+  },
+}
